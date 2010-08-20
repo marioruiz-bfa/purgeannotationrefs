634503f5f42298d59593b20caf0952feca391e86
Peter Fichtner <pfichtner@users.noreply.github.com>
2010-08-20 14:34:14 +0000

    Matcher interface declares two standard impls

--- src/org/dyndns/fichtner/purgeannotationrefs/Matcher.java
+++ src/org/dyndns/fichtner/purgeannotationrefs/Matcher.java
@@ -1,5 +1,7 @@
 package org.dyndns.fichtner.purgeannotationrefs;
 
+import java.util.regex.Pattern;
+
 /**
  * Matcher implementations decide whether a passed string matches or not.
  * 
@@ -8,11 +10,60 @@ package org.dyndns.fichtner.purgeannotationrefs;
 public interface Matcher {
 
 	/**
+	 * A RegExp based matcher (matches if the two Strings are equals).
+	 * 
+	 * @author Peter Fichtner
+	 */
+	public class StringMatcher implements Matcher {
+
+		private final String pattern;
+
+		/**
+		 * Creates a new StringMatcher that matches if the passed string is
+		 * identical to the queried one.
+		 * 
+		 * @param name the name to use
+		 */
+		public StringMatcher(final String name) {
+			this.pattern = name;
+		}
+
+		public boolean matches(final String string) {
+			return this.pattern.equals(string);
+		}
+
+	}
+
+	/**
+	 * A RegExp based matcher (matches if RegExp matches the String).
+	 * 
+	 * @author Peter Fichtner
+	 */
+	public class RegExpMatcher implements Matcher {
+
+		private final Pattern pattern;
+
+		/**
+		 * Creates a new RegExpMatcher that matches using the passed pattern.
+		 * 
+		 * @param pattern the pattern to use
+		 */
+		public RegExpMatcher(final Pattern pattern) {
+			this.pattern = pattern;
+		}
+
+		public boolean matches(final String string) {
+			return this.pattern.matcher(string).matches();
+		}
+
+	}
+
+	/**
 	 * Returns <code>true</code> if the passed String matches.
 	 * 
-	 * @param string the String to check
+	 * @param pattern the String to check
 	 * @return <code>true</code> if the passed String matches
 	 */
-	boolean matches(String string);
+	boolean matches(String pattern);
 
 }
