6df37b55daa1811495e0b3378d004f6c52afc2d8
Peter Fichtner <pfichtner@users.noreply.github.com>
2015-09-09 21:39:36 +0000

    type changed to List

--- maven-mojo/src/main/java/org/dyndns/fichtner/purgeannotationrefs/mojo/Remove.java
+++ maven-mojo/src/main/java/org/dyndns/fichtner/purgeannotationrefs/mojo/Remove.java
@@ -1,13 +1,15 @@
 package org.dyndns.fichtner.purgeannotationrefs.mojo;
 
+import java.util.List;
+
 import org.apache.maven.plugins.annotations.Parameter;
 
 public class Remove {
 
-	@Parameter(property = PurgeAnnoRefsMojo.PAR + ".removes", required = true)
+	@Parameter(property = PurgeAnnoRefsMojo.PAR + ".remove.regexp", required = true)
 	public String regexp;
 
-	@Parameter(property = PurgeAnnoRefsMojo.PAR + ".remove.removeFrom")
-	public String[] removeFroms;
+	@Parameter(property = PurgeAnnoRefsMojo.PAR + ".remove.removeFroms")
+	public List<String> removeFroms;
 
 }
