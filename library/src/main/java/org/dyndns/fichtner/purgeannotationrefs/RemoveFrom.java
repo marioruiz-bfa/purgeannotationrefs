710650d91409ddf03cb69b362fc53274ae02d335
Peter Fichtner <pfichtner@users.noreply.github.com>
2015-09-12 09:38:38 +0000

    javadoced

--- library/src/main/java/org/dyndns/fichtner/purgeannotationrefs/RemoveFrom.java
+++ library/src/main/java/org/dyndns/fichtner/purgeannotationrefs/RemoveFrom.java
@@ -1,7 +1,42 @@
 package org.dyndns.fichtner.purgeannotationrefs;
 
+import java.lang.annotation.ElementType;
+
+/**
+ * Specify the elements the annotations should be removed from. At the moment
+ * supported types are:
+ * <ol>
+ * <li>{@value #TYPES}</li>
+ * <li>{@value #FIELDS}</li>
+ * <li>{@value #CONSTRUCTORS}</li>
+ * <li>{@value #METHODS}</li>
+ * <li>{@value #PARAMETERS}</li>
+ * </ol>
+ * <br/>
+ * Local variables are not supported since they are not retained in java
+ * bytecode (class files), see <a href=
+ * "https://docs.oracle.com/javase/specs/jls/se6/html/interfaces.html#9.6.1.2"
+ * >JLS 9.6.1.2</a>
+ * 
+ * @author Peter Fichtner
+ */
 public enum RemoveFrom {
 
-	TYPES, CONSTRUCTORS, METHODS, FIELDS, PARAMETERS;
+	/**
+	 * @see ElementType#TYPE
+	 */
+	TYPES, /**
+	 * @see ElementType#FIELD
+	 */
+	FIELDS, /**
+	 * @see ElementType#CONSTRUCTOR
+	 */
+	CONSTRUCTORS, /**
+	 * @see ElementType#METHOD
+	 */
+	METHODS, /**
+	 * @see ElementType#PARAMETER
+	 */
+	PARAMETERS;
 
 }
